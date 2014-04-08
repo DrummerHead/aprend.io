d46c0bade39fe210b64a86aac55e96fadbce88c8
DrummerHead <drummerhead@gmail.com>
2014-04-08 16:53:14 -0300

    Bring youtube iframe, play proper video, adjust ratio

--- source/javascripts/main.js
+++ source/javascripts/main.js
@@ -1,1 +1,26 @@
-//= require_tree .
+document.addEventListener('DOMContentLoaded', function(){
+'use strict';
+
+
+
+
+// Set correct video ratio
+//
+var video = document.getElementById('video');
+
+if(document.body.contains(video)){
+  var youtubeIframe = video.querySelector('iframe');
+
+  var setVideoRatio = function(){
+    var videoWidth = video.clientWidth;
+    youtubeIframe.setAttribute('style', 'height: ' + Math.floor(videoWidth / (1728/1080) + 30) + 'px');
+  };
+
+  setVideoRatio();
+  window.addEventListener('resize', setVideoRatio);
+}
+
+
+
+
+});
