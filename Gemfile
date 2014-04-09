c8720bcd28c42b84bf0051ccf9ec4edd94a89a76
DrummerHead <drummerhead@gmail.com>
2014-04-09 20:45:07 -0300

    Add minify_html, activate minify_css & minify_javascript

--- Gemfile
+++ Gemfile
@@ -6,6 +6,8 @@ gem "middleman", "~>3.2.2"
 
 # Live-reloading plugin
 gem "middleman-livereload", "~> 3.1.0"
+gem "middleman-minify-html"
+
 
 # For faster file watcher updates on Windows:
 gem "wdm", "~> 0.1.0", :platforms => [:mswin, :mingw]
--- config.rb
+++ config.rb
@@ -126,11 +126,9 @@ set :haml, { :attr_wrapper => "\"", :format => :html5 }
 
 # Build-specific configuration
 configure :build do
-  # For example, change the Compass output style for deployment
-  # activate :minify_css
-
-  # Minify Javascript on build
-  # activate :minify_javascript
+  activate :minify_html
+  activate :minify_css
+  activate :minify_javascript
 
   # Enable cache buster
   # activate :asset_hash
