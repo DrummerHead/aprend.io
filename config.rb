630a68dfcc97aa2dc958a567b93ed28d09eaa927
DrummerHead <drummerhead@gmail.com>
2014-04-12 17:55:25 -0300

    Delete default comments on config.rb

--- config.rb
+++ config.rb
@@ -1,33 +1,6 @@
 ###
-# Compass
-###
-
-# Change Compass configuration
-# compass_config do |config|
-#   config.output_style = :compact
-# end
-
-###
 # Page options, layouts, aliases and proxies
 ###
-
-# Per-page layout changes:
-#
-# With no layout
-# page "/path/to/file.html", :layout => false
-#
-# With alternative layout
-# page "/path/to/file.html", :layout => :otherlayout
-#
-# A path which all have the same layout
-# with_layout :admin do
-#   page "/admin/*"
-# end
-
-# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
-#  :which_fake_page => "Rendering a fake page with a local variable" }
-
 
 page "/index.html", :layout => :landing
 
@@ -90,36 +63,14 @@ end
 
 
 
-
-
-
-
-
-
-
-
-
-
 ###
 # Helpers
 ###
-
-# Automatic image dimensions on image_tag helper
-# activate :automatic_image_sizes
 
 # Reload the browser automatically whenever files change
 activate :livereload do |livereload|
   livereload.host = Socket.ip_address_list.detect{|intf| intf.ipv4_private?}.ip_address
 end
-
-
-
-# Methods defined in the helpers block are available in templates
-# helpers do
-#   def some_helper
-#     "Helping"
-#   end
-# end
 
 set :css_dir, 'stylesheets'
 
@@ -129,18 +80,12 @@ set :images_dir, 'images'
 
 set :haml, { :attr_wrapper => "\"", :format => :html5 }
 
+
+
+
 # Build-specific configuration
 configure :build do
   activate :minify_html
   activate :minify_css
   activate :minify_javascript
-
-  # Enable cache buster
-  # activate :asset_hash
-
-  # Use relative URLs
-  # activate :relative_assets
-
-  # Or use a different image path
-  # set :http_prefix, "/Content/images/"
 end
